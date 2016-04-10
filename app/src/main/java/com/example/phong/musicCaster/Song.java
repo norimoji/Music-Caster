bf44f6fd584b3275bd11b852501c693667fc3322
Phong <zimmerzing@gmail.com>
2016-04-10 20:58:30 +0100

    Added a method to return the album art

--- app/src/main/java/com/example/phong/musicCaster/Song.java
+++ app/src/main/java/com/example/phong/musicCaster/Song.java
@@ -1,6 +1,8 @@
 package com.example.phong.musicCaster;
 
 
+import android.graphics.Bitmap;
+
 /**
  * Created by Phong on 19/11/2015.
  */
@@ -9,11 +11,13 @@ public class Song {
     private long songID;
     private String songTitle;
     private String songArtist;
+    private Bitmap albumArtwork = null;
 
-    public Song(long ID, String title, String artist) {
+    public Song(long ID, String title, String artist, Bitmap artwork){
         songID = ID;
         songTitle = title;
         songArtist = artist;
+        albumArtwork = artwork;
 
     }
 
@@ -29,6 +33,7 @@ public class Song {
         return songArtist;
     }
 
+    public Bitmap getArtWork(){return albumArtwork;}
 }
 
 
